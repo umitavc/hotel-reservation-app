0bf69a1bd4df3e0c57c1267e2d84d8b4bcc86f05
umitavc <umitavc62@gmail.com>
2024-01-21 23:42:20 +0300

    Initial commit

---
+++ lib/features/shared/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const scaffoldBgColor = Color(0xFF23272E);
+  static  const bottomnavbarcolor= Color(0xff581c21);
+  static const secondaryColor = Color(0xFF0D3B66);
+  static const appBarColor = Color(0xFF4c5b66);
+  static const buttonColor = Color(0xFFd36135);
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hotel_reservation_app/core/routers/app_router.dart';
+import 'package:hotel_reservation_app/features/home/home_screen.dart';
+import 'package:hotel_reservation_app/features/shared/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    final appRouter = AppRouter();
+    
+    return MultiBlocProvider(
+      providers: [
+        // BlocProvider<BottomNavBarBloc>(
+        //   create: (_) => BottomNavBarBloc(),
+        // ),
+       
+        // Diğer BlocProvider'ları buraya ekleyin.
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().themeData,
+        title: 'Hotel Reservation Case',
+        routerConfig: appRouter.config(),
+      ),
+    );
+  }
+}
+
